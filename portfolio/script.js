1ccd3734d5f7dfd5668a32bc09f5b7e389197b10
DevEnvironment7 <130925883+DevEnvironment7@users.noreply.github.com>
2023-04-24 19:58:46 +0200

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Froned Developer", "Youtube","Dawid","David"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
