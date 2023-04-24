3c9657492ea63a72e8e8ff11bce6be5037e0c821
DevEnvironment7 <130925883+DevEnvironment7@users.noreply.github.com>
2023-04-24 20:55:07 +0200

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text",{
-    strings: ["Froned Developer", "Youtube","Dawid","David"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
